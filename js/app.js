0e585bab18c8d060a4d1b107e9c27ff4c451f086
Damian Serrano Thode <dsthode@gmail.com>
2014-06-24 22:17:12 +0200

    Additions

---
+++ js/app.js
@@ -0,0 +1,3 @@
+angular.module('color-guess', [])
+.constant('MAX_INITIAL_COLORS', 4)
+;
--- js/game-service.js
+++ js/game-service.js
@@ -1,3 +1,27 @@
 angular.module('color-guess')
-.service('GameService', ['Colors', function(Colors) {
+.service('GameService', ['MAX_INITIAL_COLORS', 'Colors', function(Colors) {
+	var max_colors = MAX_INITIAL_COLORS;
+	var score = 0;
+	function randomNumber(max) {
+		return Math.floor(Math.random() * max);
+	};
+	return {
+		getNexColor: function() {
+			var color_idx = randomNumber(max_colors);
+			var name_idx = randomNumber(max_colors);
+			score += 1;
+			return {
+				color: Colors[color_idx].color,
+				name: Colors[name_idx].name,
+				answer: color_idx == name_idx
+			};
+		},
+		newGame: function() {
+			max_colors = MAX_INITIAL_COLORS;
+			score = 0;
+		},
+		getScore: function() {
+			return score;
+		}
+	};
 }]);
